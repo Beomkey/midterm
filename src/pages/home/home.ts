01e17fe19f315bc8dc4a1ae267ea331fe7293a49
Beom <21300356@handong.edu>
2017-10-17 12:44:48 +0900

    7

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,8 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   family: Array<Object> = [];
+  searchQuery: string = '';
+  searchRes: Array<Object> = [];
     
   constructor(public navCtrl: NavController) {
     this.family = [
@@ -15,6 +17,7 @@ export class HomePage {
       {name:'Sister', phone_number: '010-1234-0002', address: '경북 포항시 북구 흥해읍 한동로 558 한동대학교 창조관', status: 'default'},
       {name:'Brother', phone_number: '010-1234-0003', address: '경북 포항시 북구 흥해읍 한동로 558 한동대학교 비전관', status: 'default'}
       ];
+    this.searchRes = this.family;
   }
 
   pushFam() {
@@ -32,7 +35,29 @@ export class HomePage {
   goFamDetails(theFam: any) {
     this.navCtrl.push("FamDetailsPage", { fam: theFam });
   }
+  initializeItems() {
+    this.searchRes = this.family;
+  }
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.searchRes = this.searchRes.filter((item: Fam) => {
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
 }
+
+
+
+
+
 
 export class Fam {
   name: string;
